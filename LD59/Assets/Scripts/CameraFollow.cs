e4c083e602b6a1ec90f076ab0fa56b8fe2f50174
smilewood <milewood@gmail.com>
2026-04-17 23:50:36 -0600

    Basic project setup
    Not much time night 1: set up basic scene, player control, camera follow that seems a bit shaky for some reason.

---
+++ LD59/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+   public Transform target;
+
+   public float bounciness;
+
+   private Vector3 InitialOffset;
+
+   // Start is called once before the first execution of Update after the MonoBehaviour is created
+   void Start()
+   {
+      InitialOffset = this.transform.position - target.position;
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+      this.transform.position = Vector3.Lerp(this.transform.position, target.position + InitialOffset, bounciness);
+   }
+}
---
+++ LD59/Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMove : MonoBehaviour
+{
+   InputAction moveAction;
+
+   public float speed = 1;
+
+   // Start is called once before the first execution of Update after the MonoBehaviour is created
+   void Start()
+   {
+      moveAction = InputSystem.actions.FindAction("Move");
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+      Vector2 delta = moveAction.ReadValue<Vector2>().normalized * speed * Time.deltaTime;
+      this.transform.position = this.transform.position + new Vector3(delta.x, delta.y, this.transform.position.z);
+   }
+}
